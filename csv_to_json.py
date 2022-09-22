00856d7cf19ff8318cdcc1e12140020bba878fd4
KarimovProject <abdullatifkarimov772@gmail.com>
2022-09-22 09:57:01 +0500

    failed

--- csv_to_json.py
+++ csv_to_json.py
@@ -17,11 +17,11 @@ def csv_to_json(data:list)-> None:
     # Create Table
     table = db.table(name='Mobile')
 
-    # filtrs
-    ids, model, company, price = data[0]
+    # failed
+    model, company, price = data[0]
 
     for i in data[1:]:
-        # ponename
+        # Ponename
         ponename = ' '.join(i[1].split()[:2])
         # Documents
         doc = Document(value={model:ponename, company:i[2], price:i[3]}, doc_id=i[0])
